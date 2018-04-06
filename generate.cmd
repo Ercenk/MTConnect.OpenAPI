1ad275355df4d578837ed206f5659c22bc2d2018
Ercenk Keresteci <ercenk@microsoft.com>
2018-04-06 13:15:33 -0700

    New repo for the spec

---
+++ generate.cmd
@@ -0,0 +1,3 @@
+docker run --rm --name autorest -v "%cd%"\out:/out -v "%cd%"\:/input azuresdk/autorest autorest --input-file=/input/openAPI_1.3.1.yaml --csharp --output-folder=/out --namespace=MTConnect.Client
+
+
