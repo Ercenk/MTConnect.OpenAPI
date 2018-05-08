bf0267861236abff716e25ad3000af3a018d615e
Ercenk Keresteci <ercenk@microsoft.com>
2018-05-07 21:01:16 -0700

    Fixed bug on the YAML spec

--- generate.cmd
+++ generate.cmd
@@ -1,3 +1,5 @@
-docker run --rm --name autorest -v "%cd%"\out:/out -v "%cd%"\:/input azuresdk/autorest autorest --input-file=/input/openAPI_1.3.1.yaml --csharp --output-folder=/out --namespace=MTConnect.Client
-
+mkdir %temp%\input
+copy .\openAPI_1.3.1.yaml %temp%\input
+docker run --rm --name autorest -v "%cd%"/out:/out -v "%temp%"/input/:/input azuresdk/autorest autorest --input-file=/input/openAPI_1.3.1.yaml --csharp --output-folder=/out --namespace=MTConnect.Client
+rmdir /s /q %temp%\input
 
